3eac185d9bcd0b1c2ced45afe81f054f7619d541
rpaskalev <68789012+rpaskalev@users.noreply.github.com>
2022-10-31 17:31:13 -0400

    Update s3.tf

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,20 +1,110 @@
+resource "aws_s3_bucket" "dev_bucket" {
+  bucket = "development-rady-bucket-ziyotek-${data.aws_caller_identity.current.account_id}"
 
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
 
-resource "aws_s3_bucket" "iqies_my_first_resourse" {
-  bucket = "${var.bucket_name}-${data.aws_region.current.id}" #-${count.index}"
-  #acl    = var.acl_type
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
 
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
   }
-
-  provisioner "local-exec" {
-    when = destroy
-    #command = "echo ${self.id} > testfile.txt"
-    command = "aws s3 rm s3://${self.id} --recursive"
-
-    #command = "aws s3api delete-objects --bucket ${self.id} --delete \"$(aws s3api list-object-versions --bucket ${self.id} --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')"\s"
-  }
+  force_destroy = true
 }
 
+resource "aws_s3_bucket_object" "dev" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.dev_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+
+}
+
+resource "aws_s3_bucket" "prod_bucket" {
+  bucket = "production-${random_pet.petname.id}-${data.aws_region.current.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::production-${random_pet.petname.id}-${data.aws_region.current.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "prod" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.prod_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+}
+
+
+resource "aws_s3_bucket" "qa_bucket" {
+  bucket = "qa-${random_pet.petname.id}-${data.aws_region.current.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::qa-${random_pet.petname.id}-${data.aws_region.current.id}/*"
+            ]
+        }
+    ]
+}
+EOF
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "qa" {
+  key          = "index.html"
+  bucket       = aws_s3_bucket.qa_bucket.id
+  content      = file("../assets/index.html")
+  content_type = "text/html"
+}
